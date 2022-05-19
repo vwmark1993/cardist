c5427ec0b706b0695470b6604aebc673f291de57
vmark1993 <vwmark1993@gmail.com>
2022-05-18 21:35:05 -0400

    created user header

---
+++ src/components/UserHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3 class="text-2xl text-left">Header</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserHeader',
+  props: {
+    
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <UserHeader />
+    <h1 class="font-bold text-5xl text-left">Home</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserHeader from '@/components/UserHeader.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    UserHeader
+  }
+}
+</script>
