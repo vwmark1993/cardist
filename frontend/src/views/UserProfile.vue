21dcb44b088ea932884b9b720be900306c8f2a84
vmark1993 <vwmark1993@gmail.com>
2022-07-27 00:09:27 -0400

    set up user profile page structure

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <UserHeader />
-    <div class="grid grid-cols-12 grid-flow-col">
+    <div class="grid grid-cols-12 grid-flow-col items-start">
       <div class="col-span-3 m-3 p-3 border border-slate-400 rounded">
         <div class="mb-2">
           <img v-if="user.picture != null" :src="user.picture" class="rounded m-auto" />
@@ -27,25 +27,117 @@
         </div>
       </div>
       <div class="col-span-9">
-        <div class="border rounded border-slate-400 m-3 p-3">
+        <!-- Item Listings -->
+        <div class="user-profile-group-container border rounded border-slate-400 bg-white m-3 p-3">
           <div>
-            <span class="text-left text-xl font-bold">Order History</span>
+            <span class="block text-left text-xl font-bold ml-3">Current Item Listings</span>
           </div>
-          <div class="shopping-cart-item-container border rounded border-slate-400 p-3 m-3 flex">
-            <div class="flex items-center mr-3">
-              <img src="https://cdn.britannica.com/95/124395-004-3B484C8B/hand-cards-trump-spades.jpg" class="shopping-cart-item-image border rounded m-auto" />
-            </div>
-            <div class="w-full truncate relative">
-              <div class="shopping-cart-item-header flex justify-between items-center border-b border-slate-400 mb-1">
-                <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
-                <button class="shopping-cart-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Remove</button>
+          <div class="user-profile-scroll-container">
+            <div class="user-profile-item-container border rounded border-slate-300 bg-slate-100 p-3 m-3 flex">
+              <div class="flex items-center mr-3">
+                <img src="https://cdn.britannica.com/95/124395-004-3B484C8B/hand-cards-trump-spades.jpg" class="user-profile-item-image border rounded m-auto" />
               </div>
-              <span class="block text-left text-xs">Ordered On: 2022-07-24</span>
-              <span class="block text-left">$100</span>
-              <div class="bottom-0 mb-1 absolute">
-                <div class="flex flex-row items-center">
-                  <span class="text-sm">Quantity: 2</span>
+              <div class="w-full relative">
+                <div class="user-profile-item-header flex justify-between items-center border-b border-slate-400 mb-1">
+                  <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
+                  <div>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 mr-1 rounded">Details</button>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Remove</button>
+                  </div>
                 </div>
+                <span class="block text-left text-xs mb-1">Posted on: 2022-07-24</span>
+                <span class="block text-left">Total Earnings: $100</span>
+                <div class="bottom-0 mb-1 absolute">
+                  <div class="flex flex-row items-center">
+                    <span class="text-sm">Unit Sales: 14</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Order History -->
+        <div class="user-profile-group-container border rounded border-slate-400 bg-white m-3 p-3">
+          <div>
+            <span class="block text-left text-xl font-bold ml-3">Order History</span>
+          </div>
+          <div class="user-profile-scroll-container">
+            <div class="user-profile-item-container border rounded border-slate-300 bg-slate-100 p-3 m-3 flex">
+              <div class="flex items-center mr-3">
+                <img src="https://cdn.britannica.com/95/124395-004-3B484C8B/hand-cards-trump-spades.jpg" class="user-profile-item-image border rounded m-auto" />
+              </div>
+              <div class="w-full relative">
+                <div class="user-profile-item-header flex justify-between items-center border-b border-slate-400 mb-1">
+                  <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
+                  <div>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 mr-1 rounded">Details</button>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Remove</button>
+                  </div>
+                </div>
+                <span class="block text-left text-xs mb-1">Ordered on: 2022-07-24</span>
+                <span class="block text-left">Total Price: $100</span>
+                <div class="bottom-0 mb-1 absolute">
+                  <div class="flex flex-row items-center">
+                    <span class="text-sm">Quantity: 2</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="user-profile-item-container border rounded border-slate-300 bg-slate-100 p-3 m-3 flex">
+              <div class="flex items-center mr-3">
+                <img src="https://cdn.britannica.com/95/124395-004-3B484C8B/hand-cards-trump-spades.jpg" class="user-profile-item-image border rounded m-auto" />
+              </div>
+              <div class="w-full relative">
+                <div class="user-profile-item-header flex justify-between items-center border-b border-slate-400 mb-1">
+                  <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
+                  <div>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 mr-1 rounded">Details</button>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Remove</button>
+                  </div>
+                </div>
+                <span class="block text-left text-xs mb-1">Ordered on: 2022-07-24</span>
+                <span class="block text-left">Total Price: $100</span>
+                <div class="bottom-0 mb-1 absolute">
+                  <div class="flex flex-row items-center">
+                    <span class="text-sm">Quantity: 2</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Comments -->
+        <div class="user-profile-group-container border rounded border-slate-400 bg-white m-3 p-3">
+          <div>
+            <span class="block text-left text-xl font-bold ml-3">Comments</span>
+          </div>
+          <div class="user-profile-scroll-container">
+            <div class="user-profile-item-container border rounded border-slate-300 bg-slate-100 p-3 m-3 flex">
+              <div class="w-full">
+                <div class="user-profile-item-header flex justify-between items-center border-b border-slate-400 mb-1">
+                  <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
+                  <div>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 mr-1 rounded">Edit</button>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Remove</button>
+                  </div>
+                </div>
+                <span class="block text-left text-xs mb-1">Posted on: 2022-07-24</span>
+                <span class="block text-left line-clamp-3">Cool I guess</span>
+              </div>
+            </div>
+            <div class="user-profile-item-container border rounded border-slate-300 bg-slate-100 p-3 m-3 flex">
+              <div class="w-full">
+                <div class="user-profile-item-header flex justify-between items-center border-b border-slate-400 mb-1">
+                  <h6 class="text-lg truncate">Bicycle Playing Cards</h6>
+                  <div>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 mr-1 rounded">Edit</button>
+                    <button class="user-profile-item-remove-button bg-slate-500 hover:bg-slate-700 text-white text-sm px-3 py-1 rounded">Delete</button>
+                  </div>
+                </div>
+                <span class="block text-left text-xs mb-1">Posted on: 2022-07-24</span>
+                <span class="block text-left line-clamp-3">Cool I guess</span>
               </div>
             </div>
           </div>
@@ -104,22 +196,28 @@ export default {
 }
 </script>
 <style scoped>
-    .shopping-cart-item-container {
-    min-width: 00px;
-    min-height: 166px;
+.user-profile-group-container {
+    height: auto;
+  }
+  .user-profile-scroll-container {
+    max-height: 195px;
+    overflow: auto;
+  }
+  .user-profile-item-container {
+    height: 166px;
   }
 
-  .shopping-cart-item-input-field {
+  .user-profile-item-input-field {
     height: 30px;
     width: 40px;
   }
 
-  .shopping-cart-item-image {
-    max-width: 150px;
+  .user-profile-item-image {
+    max-width: 144px;
   }
 
-  .shopping-cart-item-header {
-    height: 52px;
+  .user-profile-item-header {
+    height: 46px;
   }
 
 </style>
